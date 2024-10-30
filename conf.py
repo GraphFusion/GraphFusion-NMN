00ae2c46eb437c539a9b4052c1680ea1010cc5e8
Kiplangat Korir <korirkiplangat22@gmail.com>
2024-10-30 18:19:14 +0300

    new

--- conf.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-project_root = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(project_root)
-
-print("Project Root:", project_root)
-print("sys.path:", sys.path)  # This will print out all paths where Python looks for modules
